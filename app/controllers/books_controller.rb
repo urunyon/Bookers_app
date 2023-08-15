1a2a2ebf3e75094f298916917843c911433cb823
urunyon <141293154+urunyon@users.noreply.github.com>
2023-08-15 11:00:22 +0000

    [Finish]Bookers_app

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,34 +4,45 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+    @book = Book.new(book_params)
+    if @book.save
+       flash[:notice] = "Book was successfully created."
+       redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      flash.now[:notice] = "error prohibited this book from being saved:"
+      render :index
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
-    @book = Book.find(params[:id])
+    @books = Book.find(params[:id])
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to '/books'
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] = "Book was successfully update."
+       redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
-  end  
+  end
 
   private
   def book_params
